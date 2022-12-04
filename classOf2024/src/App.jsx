029889fc680b3bdae8ed0cc67df108c020cfddb8
himTresor1 <amizeropeace@gmail.com>
2022-12-04 19:01:44 +0200

    Tresor

---
+++ classOf2024/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import Tresor from "./components/Tresor";
+
+function App() {
+  return (
+    <div className="App">
+      <Tresor />
+    </div>
+  );
+}
+
+export default App;
---
+++ classOf2024/src/components/Tresor.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "../App.css";
+ 
+
+ function Tresor(){
+    return(
+                    <div className= "h-screen w-full bg-orange-100 flex flex-row  ">
+                        <div className="w-[50%] bg-red-100 ">
+                            <div className=" p">
+                                <h1>AMIZERO TRESOR</h1>
+                            </div>
+                        </div>
+                        <div className="w-[50%] bg-blue-100">asdf</div>
+                    </div>
+    )
+}
+
+     export default Tresor
