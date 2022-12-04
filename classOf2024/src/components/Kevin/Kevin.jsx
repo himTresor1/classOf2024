6612705020c019ca6eb0b2cd74d1b90994c617bc
JoyKevin1 <banjoflylive@gmail.com>
2022-12-04 21:14:37 +0200

    ...

--- classOf2024/src/components/Kevin/Kevin.jsx
+++ classOf2024/src/components/Kevin/Kevin.jsx
@@ -2,19 +2,24 @@ import React from "react";
 
 const Kevin = () => {
   return (
-    <div className="h-[100vh] w-[75%] flex items-center bg-yellow-100">
-      <div className="w-[20%] h-[70%] bg-red-700"></div>
+    <div className="h-[100vh] w-[75%] flex items-center bg-black">
+      <div className="w-[20%] h-[65%] bg-red-700"></div>
       <div className=" text-[1.2rem] font-semibold text-white w-[80%] h-full bg-blue-200">
-        <div className="bg-yellow-400 h-[90%] flex justify-around">
-            <div className="w-[40%] bg-gray-200">
-                <h3 className="font-semibold text-[#CEEF06] text-[2rem]">TYPE MASTER</h3>
-                <h2 className="">Kevin</h2>
-                <h2 className="">Banjo</h2>
-            </div>
-            <div className="w-[50%] bg-red-300 flex flex-col justify-between items-center">
-                <div className="h-[28rem] w-[25rem] bg-[yellow]"></div>
-                <div className="h-[20rem] w-[20rem] bg-[red]"></div>
-            </div>
+        <div className="bg-yellow-400 h-[90%] flex justify-around items-center">
+          <div className="h-[80%] w-[40%] bg-gray-200 flex flex-col items-center">
+            <h3 className="font-semibold text-[#CEEF06] text-[2rem]">
+              TYPE MASTER
+            </h3>
+            <h2 className="text-[9rem]">Kevin</h2>
+            <h2 className="text-[9rem]">Banjo</h2>
+            <button className="outline-none h-[4rem] w-[13.5rem] bg-white text-black text-[1rem]">
+              PORTFOLIO
+            </button>
+          </div>
+          <div className="w-[50%] h-full bg-red-300 flex flex-col justify-around items-end">
+            <div className="h-[27rem] w-[25rem] bg-[yellow]"></div>
+            <div className="h-[24rem] w-[27rem] bg-[red]"></div>
+          </div>
         </div>
         <h2>Any fool can write code that a computer can understand.</h2>
         <h2>
