87b473c89a4498b8df6c5c29045e64f5879835e3
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-12-05 12:12:12 +0200

    fix: tr overflow errors

--- classOf2024/src/components/TRESOR/Tresor.jsx
+++ classOf2024/src/components/TRESOR/Tresor.jsx
@@ -7,9 +7,9 @@ function Tresor() {
   return (
     <div className="h-[1090px] w-screen bg-[#F9F3F1] flex flex-row ">
       <div className="w-[50%]  ">
-        <div className="   w-[90%]  mt-[18rem] ml-[10rem] ">
+        <div className="   w-[90%]  mt-[18rem] ml-[7rem] ">
           <h1 className="font-bold text-[2rem]">AMIZERO TRESOR</h1>
-          <div className=" w-[95%]  stand">
+          <div className=" w-[95%]">
             <h1 className="font-bold text-[7rem]">Stand with pride. </h1>
           </div>
           <h1 className="font-semibold text-[1rem]">
@@ -20,7 +20,7 @@ function Tresor() {
           <button className=" ml-[40%]  mt-[5rem] w-[13rem] h-[4rem] bg-black  outline-none border-[2px] border-black text-white font-bold">PORTFOLIO</button>
         </div>
       </div>
-      <div className="w-[50%]">
+      <div className="w-[50%] mb-[2rem]">
         <img className="  ml-[6rem] mt-[3rem]  h-[90%]" src={tresor1} alt="" />
       </div>
 
