38e10f264d2ddff3589e5ce00726c275593f9e2a
Amizero Tresor <115831730+himTresor1@users.noreply.github.com>
2024-01-26 10:00:12 +0200

    Kevin (#3)

--- classOf2024/src/App.jsx
+++ classOf2024/src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from "react";
 import "../src/App.css"
 import Tresor from "./components/TRESOR/Tresor";
 import Pacifiquem from "./components/Pacifiquem/Mpac";
+import Kevin from "./components/Kevin/Kevin";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App flex flex-col items-center">
       <Tresor />
+      <Kevin />
       <Pacifiquem />
     </div>
   );
